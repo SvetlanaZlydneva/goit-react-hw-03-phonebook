c0005aad32bf4b2cfc9a29181bf3de3d04861e6e
Svetlana Zlydneva <svetlanaderiglazova87@gmail.com>
2020-06-23 19:23:01 +0300

    init

---
+++ src/components/Container/Container.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Container.module.css';
+
+class Container extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  render() {
+    const { children } = this.props;
+    return <div className={styles.container}>{children}</div>;
+  }
+}
+
+export default Container;
